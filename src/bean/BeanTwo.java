c6e1e154cced3519859112ba2781ddf0ce8dc1c9
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-08-20 00:36:10 +0530

    another bean class add

---
+++ src/bean/BeanTwo.java
@@ -0,0 +1,10 @@
+package bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanTwo {
+    public BeanTwo() {
+        System.out.println("Object create Bean Two ");
+    }
+}
