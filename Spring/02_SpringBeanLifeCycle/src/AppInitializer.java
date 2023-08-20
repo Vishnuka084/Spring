190f01c9f03f309d02baac9f0778fc4b3f93afe0
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-08-21 01:08:22 +0530

    Updated Content

---
+++ Spring/02_SpringBeanLifeCycle/src/AppInitializer.java
@@ -0,0 +1,16 @@
+import config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
+
+        ctx.registerShutdownHook();
+
+
+
+
+    }
+}
---
+++ Spring/02_SpringBeanLifeCycle/src/bean/SpringBeanOne.java
@@ -0,0 +1,11 @@
+package bean;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBeanOne {
+    public SpringBeanOne() {
+        System.out.println("Spring Bean one ");
+    }
+}
